b024219070acf96a26199a5ae611ac72ec7c2eaf
kakeru <kakeru.kageshima@bootware.jp>
2021-07-14 22:14:17 +0900

    Selection 機能の追加

--- src/TestMain.java
+++ src/TestMain.java
@@ -5,7 +5,7 @@ public class TestMain {
 
   public static void main(String[] args) throws IOException {
 
-    File targetFile = new File("C:\\Users\\kakeru\\Workspace\\GEMS_2\\ifile-testing\\src\\test\\SampleClass.java");
+    File targetFile = new File("ifile-testing/src/test/SampleClass.java");
 
     // targetFile の 6行目から8行目を抽出する場合
     RowSelection selection = RowSelectionAction.run(targetFile, 6, 8);
